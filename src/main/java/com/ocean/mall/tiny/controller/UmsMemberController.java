720c38a2992fa1de923388d3e3cb46003b9b9be6
ocean2580 <2080917477@qq.com>
2023-05-12 21:25:31 +0800

    add Redis cache

---
+++ src/main/java/com/ocean/mall/tiny/controller/UmsMemberController.java
@@ -0,0 +1,40 @@
+package com.ocean.mall.tiny.controller;
+
+import com.ocean.mall.tiny.common.api.CommonResult;
+import com.ocean.mall.tiny.service.UmsMemberService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+/**
+ * @version 1.0
+ * @Author Oliver
+ * @Date 2023/5/12 21:20
+ * @注释
+ */
+@Controller
+@Api(tags = "UmsMemberController", description = "会员登录注册管理")
+@RequestMapping("/sso")
+public class UmsMemberController {
+    @Autowired
+    private UmsMemberService memberService;
+
+    @ApiOperation("获取验证码")
+    @RequestMapping(value = "/getAuthCode", method = RequestMethod.GET)
+    @ResponseBody
+    public CommonResult getAuthCode(@RequestParam String telephone) {
+        return memberService.generateAuthCode(telephone);
+    }
+
+    @ApiOperation("判断验证码是否正确")
+    @RequestMapping(value = "/verifyAuthCode", method = RequestMethod.POST)
+    @ResponseBody
+    public CommonResult updatePassword(@RequestParam String telephone,
+                                       @RequestParam String authCode) {
+        return memberService.verifyAuthCode(telephone,authCode);
+    }
+}
