7ece9501e9270c65741b5a9a6242f22d1bd474be
ocean2580 <2080917477@qq.com>
2023-05-12 19:45:40 +0800

    init repo

---
+++ src/main/java/com/ocean/malltinylearning/MallTinyLearningApplication.java
@@ -0,0 +1,13 @@
+package com.ocean.malltinylearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallTinyLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallTinyLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ocean/malltinylearning/MallTinyLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ocean.malltinylearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallTinyLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
