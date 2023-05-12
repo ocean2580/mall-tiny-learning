faf1629e37425acd5300a73def60540699532074
ocean2580 <2080917477@qq.com>
2023-05-12 20:32:57 +0800

    add MyBatis Generator

--- src/main/java/com/ocean/mall/tiny/MallTinyLearningApplication.java
+++ src/main/java/com/ocean/mall/tiny/MallTinyLearningApplication.java
@@ -1,4 +1,4 @@
-package com.ocean.malltinylearning;
+package com.ocean.mall.tiny;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/ocean/mall/tiny/MallTinyLearningApplicationTests.java
+++ src/test/java/com/ocean/mall/tiny/MallTinyLearningApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ocean.malltinylearning;
+package com.ocean.mall.tiny;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
