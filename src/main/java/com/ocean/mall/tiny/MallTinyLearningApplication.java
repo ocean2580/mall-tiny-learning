052547e9697f032ff570cc6d7491d0ed63fb02df
ocean2580 <2080917477@qq.com>
2023-05-13 11:34:51 +0800

    add SpringSecurity

--- src/main/java/com/ocean/mall/tiny/MallTinyLearningApplication.java
+++ src/main/java/com/ocean/mall/tiny/MallTinyLearningApplication.java
@@ -1,9 +1,11 @@
 package com.ocean.mall.tiny;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.ocean.mall.tiny.dao")
 public class MallTinyLearningApplication {
 
     public static void main(String[] args) {
