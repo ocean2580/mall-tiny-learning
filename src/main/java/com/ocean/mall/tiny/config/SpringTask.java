cfb08f6b2f3e0672ca066a61aaaae448f1a50eaf
ocean2580 <2080917477@qq.com>
2023-05-13 11:45:21 +0800

    add SpringTask

---
+++ src/main/java/com/ocean/mall/tiny/config/SpringTask.java
@@ -0,0 +1,15 @@
+package com.ocean.mall.tiny.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @version 1.0
+ * @Author Oliver
+ * @Date 2023/5/13 11:42
+ * @注释
+ */
+@EnableScheduling
+@Configuration
+public class SpringTask {
+}
