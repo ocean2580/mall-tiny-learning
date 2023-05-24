a1e2173aea2444026732236939df2f0335062e86
ocean2580 <2080917477@qq.com>
2023-05-24 23:02:26 +0800

    add mongodb

---
+++ src/main/java/com/ocean/mall/tiny/nosql/mongodb/repository/MemberReadHistoryRepository.java
@@ -0,0 +1,21 @@
+package com.ocean.mall.tiny.nosql.mongodb.repository;
+
+
+import com.ocean.mall.tiny.nosql.mongodb.document.MemberReadHistory;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * 会员商品浏览历史Repository
+ * Created by ocean on 2018/8/3.
+ */
+@Repository
+public interface MemberReadHistoryRepository extends MongoRepository<MemberReadHistory,String> {
+    /**
+     * 根据会员id按时间倒序获取浏览记录
+     * @param memberId 会员id
+     */
+    List<MemberReadHistory> findByMemberIdOrderByCreateTimeDesc(Long memberId);
+}
